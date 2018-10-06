fb5eeee62c400cee92aabddc6d80474daa1ae8a1
MingZhang1016 <prattkate33@gmail.com>
2018-10-06 19:08:17 -0400

    add typescript

---
+++ src/index.d.ts
@@ -0,0 +1,23 @@
+// Type definitions for react-native-gradient-buttons
+// Project: React Native Gradient Buttons
+// Definitions by: Thomas Wang <[thomaswang.io]>
+
+/*~ You can declare types that are available via importing the module */
+export interface Props {
+    text: string,
+    gradientBegin: string,
+    gradientEnd: string,
+    gradientDirection: string,
+    height: any,
+    width: any,
+    radius: number,
+    impact: boolean,
+    impactStyle: string,
+    onPressAction: Function,    
+    purpleViolet: boolean,
+    violetPink: boolean,
+    pinkDarkGreen: boolean,
+    blueViolet: boolean,
+    blueMarine: boolean,
+    deepBlue: boolean,
+}
